3d324c9e2df78c4ec96cd00fa18fedc2fa4a57fc
alexKotz-koz <64107161+alexKotz-koz@users.noreply.github.com>
2024-03-26 08:46:08 -0600

    Init commit on this brach

--- src/components/createContigs.py
+++ src/components/createContigs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from itertools import dropwhile
 class CreateContigs:
     def __init__(self, graph):
         self.graph = graph
@@ -9,8 +10,34 @@ class CreateContigs:
     # Output: a list of all start nodes (nodes that only have outgoing edges)
     def findStartNodes(self, inputGraph):
         ## TODO: Will need to optimize for larger dataset
-        #calculate the in and out degrees for 
-        sourceNodes = set()
+        #calculate the in and out degrees for each node
+
+        
+        edgesCount = {}
+        startEdges = []
+        for edge in inputGraph:
+            # first node in the tuple has an outgoing edge
+            if edge[0] in edgesCount:
+                edgesCount[edge[0]][1] += 1
+            else:
+                edgesCount[edge[0]] = [0,1] #[incoming, outgoing]
+            
+            # second node in the tuple has an incoming edge
+            if edge[1] in edgesCount:
+                edgesCount[edge[1]][0] += 1
+            else:
+                edgesCount[edge[1]] = [1,0]
+        #print("edgecount: ", edgesCount)
+        # Initialize a list to store the start edges
+
+        # Iterate over the edges
+        for edge in inputGraph:
+            # If the first node in the edge is not in edges_count or has zero incoming edges
+            if edge[0] not in edgesCount or edgesCount[edge[0]][0] == 0:
+                # Add the edge to the start_edges list
+                startEdges.append(edge)
+
+        '''sourceNodes = set()
         targetNodes = set()
         # get source and target nodes for each edge to find all nodes that only have outgoing edges
         for edge in inputGraph:
@@ -21,27 +48,38 @@ class CreateContigs:
         #print("in findStartNodes, target: ", targetNodes)
         # start nodes only exist in the source nodes, so subtracting the two sets return the start nodes
         startNodes = sourceNodes-targetNodes
-        #print('start nodes: ', startNodes)
-        return startNodes
+        #print('start nodes: ', startNodes)'''
+        return edgesCount, startEdges
 
     # Input: start node and graph (edge list)
     # Output: allPaths object that contains all possible paths through the graph
-    def followPath(self, startNode):
+    def followPath(self, startEdge):
         path = []
         # start the path with the start node returned from findStartNodes
-        target = startNode
+        target = startEdge
         # make a copy of the graph to manipulate during the path walk
         tempGraph = self.graph.copy()
-        df = pd.DataFrame(tempGraph)
+        '''df = pd.DataFrame(tempGraph)
         with open('test.csv', 'w') as file:
-            df.to_csv(file)
+            df.to_csv(file)'''
                 
         
-
+        startIndex = -1
+        for index, edge in enumerate(tempGraph):
+            if edge == startEdge:
+                startIndex = index
         
+        if startIndex == -1:
+            print("error finding start index for path walk")
+            exit
+
+
         while True:
-            # for each edge in the graph copy
-            for i, edge in enumerate(tempGraph):
+            dictIter = iter(tempGraph.items())
+            #In this code, dropwhile() skips items from the dictionary iterator until it reaches the start_key.
+            for edge, value in dropwhile(lambda edgeValue: edgeValue[0] != startEdge, dictIter):
+                print(edge)
+                target = edge[0]
                 '''if i+1<len(tempGraph) and edge[0] == tempGraph[i+2][0]:  
                     print(f"split in path @: {i} \n split @: {edge} and {tempGraph[i+1]}")'''
                 '''
@@ -75,56 +113,47 @@ class CreateContigs:
     # Output: contiguous sequences
     def createContigs(self):
         inputGraph = self.graph
-        startNodes = self.findStartNodes(inputGraph)
+        edgesCount, startEdges = self.findStartNodes(inputGraph)
         contigs = []
         contigIndexTable = {}
         #print(inputGraph)
-        
-        # using the list of starting nodes returned from findStartNodes, follow all paths through the graph. followPath() will add the path to a global object "allPaths"
-        for node in startNodes:
-            self.followPath(startNode=node)
-        '''for path in self.allPaths:
-            print(path)
-            print("\n")'''
-        '''for path in self.allPaths:
-            print(path)
-            print("\n")'''    
-        #print("all paths len: ", len(self.allPaths))
-        # for each of the paths (list of edges) in the global allPaths object
+
+        for edge in startEdges:
+            self.followPath(startEdge=edge)
+
+        '''for node in edgesCount:
+            if edgesCount[node][0] == 0:
+                self.followPath(startEdge=node)
+                print(node)'''
+
         for path in self.allPaths:
             contig = []
             contigStr = ""
             contigMetadata = []
             kmers = []
-            # for each edge in the current path
+
             for ei,edge in enumerate(path):
-                print(ei)
-                # for each node in the edge
+
                 for nodes in edge.keys():
                     metadata = path[ei]
                     for ni, node in enumerate(nodes):
                         if len(contig) == 0:
                             contig.append(node)
                             contigStr = node
-                            print('first node: ', node)
+
                         else:
                             contig.append(node[-1])
-                            print("node: ", node)
-                            print("contigSTR pre: ", contigStr)
-                            print("node last char: ", node[-1])
-                            contigStr += node[-1]
 
-                            print("contigStr post: ", contigStr)
-                            print("\n\n")
+                            contigStr += node[-1]
                             
                         if ni == 0:
                             kmer = node + nodes[1][-1]
                             
                         if ni == 1:
-                            if kmer not in contigStr:
+                            '''if kmer not in contigStr:
                                 print("NOT in contig")
                             else:
-                                print("KMER in CONTIG")
+                                print("KMER in CONTIG")'''
                             startIndex = contigStr.find(kmer)
                             stopIndex = startIndex + len(kmer)
                             contigMetadata.append({kmer: {startIndex: stopIndex}})
