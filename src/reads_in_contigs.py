5a6f9d489d7943186f13be7b62c11c037bc76926
alexKotz-koz <64107161+alexKotz-koz@users.noreply.github.com>
2024-04-04 15:58:53 -0600

    04/04/2024 15:58:53 | Fixed reads_in_contigs test

--- src/reads_in_contigs.py
+++ src/reads_in_contigs.py
@@ -15,17 +15,14 @@ def import_reads(readsFile):
 
 def numberOfReadsInContigs(readsFile):
     readsData = import_reads(readsFile=readsFile)
-    print(readsData)
     # Open the contigs file
     with open('contigs.txt', 'r') as contigs_file:
-        contigs = contigs_file.readlines()
+        contigs = [line.strip() for line in contigs_file.readlines()]
     total = []
+    print(contigs)
     # For each contig, count the number of matching reads
-    for i, contig in enumerate(contigs):
-        contig = contig.strip()  # Remove newline characters
-        
-        # Use grep to count the number of matching reads
-        if contig in readsData:
+    for read in readsData:
+        if read in contigs:
             total.append(1)
 
         # Print the number of reads for this contig
